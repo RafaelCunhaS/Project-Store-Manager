70b5e4ce3fdbd7e4c3b6130e840c307420263313
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-07 13:00:59 -0300

    chore: updates app.js with errorMiddleware and productsRouter

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const productsRouter = require('./routes/productsRouter');
+const error = require('./middlewares/errorMiddleware');
 
 const app = express();
+app.use(express.json());
 
+app.use('/products', productsRouter);
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+app.use(error);
+
 module.exports = app;
