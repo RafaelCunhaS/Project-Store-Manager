e802c52717ba7405e48027f49aca3fce2c7fdbfe
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-07 13:46:51 -0300

    feat: adds unit test and funciton getAll for salesController

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,1 +1,20 @@
-// Renomeie esse arquivo
+const SalesService = require('../services/salesService');
+const { OK_STATUS } = require('../utils/statusCode');
+
+const getAll = async (_req, res) => {
+  const result = await SalesService.getAll();
+
+  return res.status(OK_STATUS).json(result);
+};
+
+// const getById = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await SalesService.getById(id);
+
+//   return res.status(OK_STATUS).json(result);
+// };
+
+module.exports = {
+  getAll,
+  // getById,
+};
---
+++ test/unit/controllers/salesController.test.js
@@ -0,0 +1,70 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const SalesService = require('../../../services/salesService');
+const SalesController = require('../../../controllers/salesController');
+
+describe('Ao chamar a função getAll do Controller', () => {
+  describe('Se não houver vendas no BD', () => {
+
+    const response = {};
+    const request = {};
+    const payload = [];
+  
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SalesService, 'getAll').resolves(payload)
+    });
+  
+    after(() => SalesService.getAll.restore());
+
+    it('é retornado o método "status" passando o código 200', async () => {
+      await SalesController.getAll(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+    it('é retornado o método "json" passando um array', async () => {
+      await SalesController.getAll(request, response);
+
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    })
+  })
+
+  describe('Se houver vendas no BD', () => {
+
+    const response = {};
+    const request = {};
+    const payload = [{
+      saleId: 1,
+      date: '2021-09-09T04:54:29.000Z',
+      productId: 1,
+      quantity: 2
+    }]
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SalesService, 'getAll').resolves(payload)
+    });
+
+    after(() => SalesService.getAll.restore());
+
+    it('é retornado o método "status" passando o código 200', async () => {
+      await SalesController.getAll(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+    it('é retornado o método "json" passando um array', async () => {
+      await SalesController.getAll(request, response);
+
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+    it('o array possui objetos', async () => {
+      await SalesController.getAll(request, response);
+
+      expect(response.json.calledWith(sinon.match(payload))).to.be.equal(true);
+    });
+  })
+})
