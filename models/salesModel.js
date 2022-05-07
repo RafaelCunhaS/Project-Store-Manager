fddb0ba0a9ffe5982cfb276059083b8c597bf78e
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-07 14:17:25 -0300

    fix: fixed the sql query of getAll function

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,7 +1,14 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [data] = await connection.execute('SELECT * FROM sales ORDER BY saleId, productId');
+  const [data] = await connection
+    .execute(
+      `SELECT sp.sale_id AS saleId, sa.date, sp.product_id AS productId, sp.quantity
+      FROM sales AS sa 
+      JOIN sales_products AS sp
+      ON sa.id = sp.sale_id
+      ORDER BY sp.sale_id, sp.product_id`,
+    );
   return data;
 };
 
