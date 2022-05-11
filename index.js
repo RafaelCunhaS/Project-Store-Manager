da18f821d9eb43d07e278df73deec57e0ef7d192
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-11 12:00:55 -0300

    chore: remapping files

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const app = require('./app');
+const express = require('express');
+const productsRouter = require('./routes/productsRouter');
+const salesRouter = require('./routes/salesRouter');
+const error = require('./middlewares/errorMiddleware');
 require('dotenv').config();
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+const app = express();
+app.use(express.json());
+
+app.use('/products', productsRouter);
+
+app.use('/sales', salesRouter);
+
+app.use(error);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
+
+module.exports = app;
